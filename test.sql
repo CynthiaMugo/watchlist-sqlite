60f458f7e3f55420a557aaf2656f8881edcc9280
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-09-06 11:54:58 +0300

    Add initial SQLite database and configuration settings

---
+++ test.sql
@@ -0,0 +1,11 @@
+-- DROP TABLE IF EXISTS users;
+-- CREATE TABLE IF NOT EXISTS users(
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) UNIQUE NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- INSERT INTO users (name, email) VALUES ('Alice', 'alice@gmail.com')
+
+INSERT INTO users (name, email) VALUES ('Bob', 'bob@gmail.com')
